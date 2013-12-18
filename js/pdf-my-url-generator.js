e79e2b1f961e025fd8dffd9e0520f9b73be396a4
Scott Vossen <scottvossen@gmail.com>
2013-12-18 09:43:24 -0600

    Update PdfMyUrl for new buyout url

--- js/pdf-my-url-generator.js
+++ js/pdf-my-url-generator.js
@@ -6,7 +6,12 @@ var pdyMyUrlGenerator = function() {
 	return {
 		convertURI : function(url) {
 
-			window.location.assign("http://pdfmyurl.com?s=l&url=" + escape(url) + ")");
+			// current url / API
+			// window.location.assign("http://pdfmyurl.com?s=l&url=" + escape(url));
+
+			// future url / API due to buyout
+			window.location.assign("http://usa.htm2pdf.co.uk/urltopdf?apikey=yourapikey&url=" + escape(url));
+
 			// return "javascript:pdf_url=location.href;location.href='http://pdfmyurl.com?s=l&url='+escape('" + url + "')'";
 		}
 	};
